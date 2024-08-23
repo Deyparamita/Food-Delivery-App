9c732ccddb908f1297acaf5eb13039dfff5663a0
Deyparamita <deyparamita369@gmail.com>
2024-08-23 20:32:04 +0530

    Updated

--- Frontend/src/components/FoodItem/FoodItem.jsx
+++ Frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,39 +1,3 @@
-// // import React, { useContext, useState } from 'react'
-// // import './FoodItem.css'
-// // import { assets } from '../../assets/assets'
-// // import { StoreContext } from '../../context/StoreContext'
-
-// // const FoodItem = ({id,name,price,description,image}) => {
-
-// //    const [itemCount, setItemCount] = useState(0)
-// //   // const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
-
-// //   return (
-// //     <div className='food-item'>
-// //       <div className='food-item-img-container'>
-// //         <img className="food-item-image" src={image} alt="Food-Images" />
-// //         { !itemCount ? <img className='add' onClick={() => setItemCount((prev) => prev+1)} src={assets.add_icon_white} alt='add'/> 
-// //            : <div className='food-item-counter'>
-// //             <img onClick={()=>setItemCount((prev) => prev-1)} src={assets.remove_icon_red} alt="decrement-button" />
-// //             <p>{itemCount}</p>
-// //             <img onClick={()=>setItemCount((prev) => prev+1)} src={assets.add_icon_green} alt="increment-button" />
-// //            </div>
-// //         }
-// //       </div>
-// //       <div className='food-item-info'>
-// //           <div className="food-item-name-rating">
-// //             <p>{name}</p>
-// //             <img src={assets.rating_starts} alt="rating" />
-// //           </div>
-// //           <p className='food-item-desc'>{description}</p>
-// //           <p className='food-item-price'>${price}</p>
-// //       </div>
-// //     </div>
-// //   )
-// // }
-
-// // export default FoodItem
-
 import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
@@ -59,7 +23,6 @@ const FoodItem = ({id,name,price,description,image}) => {
       <div className='food-item-info'>
           <div className="food-item-name-rating">
             <p>{name}</p>
-            {/* <img src={assets.rating_starts} alt="rating" /> */}
           </div>
           <p className='food-item-desc'>{description}</p>
           <p className='food-item-price'>Rs.{price}</p>
@@ -70,40 +33,3 @@ const FoodItem = ({id,name,price,description,image}) => {
 
 export default FoodItem
 
-
-// import React, { useContext } from 'react'
-// import './FoodItem.css'
-// import { assets } from '../../assets/assets'
-// import { StoreContext } from '../../context/StoreContext'
-
-// const FoodItem = ({ id, name, price, description, image }) => {
-
-//   const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
-
-//   return (
-//     <div className='food-item'>
-//       <div className='food-item-img-container'>
-//         <img className="food-item-image" src={url + "/images/" + image} alt="Food-Images" />
-//         {!cartItems[id] ? 
-//           <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt='add' /> 
-//           : 
-//           <div className='food-item-counter'>
-//             <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="decrement-button" />
-//             <p>{cartItems[id] || 0}</p>
-//             <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="increment-button" />
-//           </div>
-//         }
-//       </div>
-//       <div className='food-item-info'>
-//         <div className="food-item-name-rating">
-//           <p>{name}</p>
-//           <img src={assets.rating_starts} alt="rating" />
-//         </div>
-//         <p className='food-item-desc'>{description}</p>
-//         <p className='food-item-price'>Rs.{price}</p>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default FoodItem
